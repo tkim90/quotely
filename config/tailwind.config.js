0404245f55a8559a7d717c45ff5beaa40857a301
TK <taehnkim@gmail.com>
2021-12-29 23:02:05 -0800

    Add navbar

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -8,6 +9,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
@@ -17,46 +22,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    plugin(function ({addComponents}) {
-      addComponents({
-        ".alert": {
-          padding: ".5rem 1rem",
-          fontWeight: "600",
-        },
-        ".alert-blue": {
-          backgroundColor: "#3490dc",
-          color: "#fff",
-          "&:hover": {
-            backgroundColor: "#2779bd",
-          },
-        },
-        ".alert-red": {
-          backgroundColor: "#e3342f",
-          color: "#fff",
-          "&:hover": {
-            backgroundColor: "#cc1f1a",
-          },
-        },
-        ".btn": {
-          padding: ".5rem 1rem",
-          borderRadius: ".25rem",
-          fontWeight: "600",
-        },
-        ".btn-blue": {
-          backgroundColor: "#3490dc",
-          color: "#fff",
-          "&:hover": {
-            backgroundColor: "#2779bd",
-          },
-        },
-        ".btn-red": {
-          backgroundColor: "#e3342f",
-          color: "#fff",
-          "&:hover": {
-            backgroundColor: "#cc1f1a",
-          },
-        },
-      });
-    })
   ],
 }
